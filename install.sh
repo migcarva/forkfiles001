a3f704f16007cf93a94a8b39fdbfb9118c90f1e6
Miguel Carvalho <miguel@oaklab.lan>
2020-09-18 02:07:55 +0100

    update flow

--- install.sh
+++ install.sh
@@ -7,4 +7,14 @@ mkdir -p $dir
 cd $dir
 git clone --recursive https://github.com/migcarva/forkfiles001.git
 cd forkfiles001
+
 sh etc/symlink-dotfiles.sh
+
+while true; do
+    read -p "Do you wish to bootstrap?" yN
+    case $yn in
+        [Yy]* ) sh etc/bootstrap-macos.sh; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
